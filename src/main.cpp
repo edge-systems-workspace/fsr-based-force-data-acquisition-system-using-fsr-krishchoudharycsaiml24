788d2eb20cd4d6cc528b30d9a6fa9bf3d353103e
krish choudhary <krish.choudhary_cs.aiml24@gla.ac.in>
2026-02-20 15:16:10 +0530

    Initialize Serial communication and print messages

--- src/main.cpp
+++ src/main.cpp
@@ -18,3 +18,15 @@ const int FSR_PIN = A0;
 // TODO 2:
 // Create variable to store sensor reading
 int fsrValue = 0;
+
+void setup() {
+
+    // TODO 3:
+    // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
+
+    // TODO 4:
+    // Print system initialization message
+    Serial.println("FSR Force Measurement System Initialized");
+    Serial.println("Monitoring force input...");
+}
